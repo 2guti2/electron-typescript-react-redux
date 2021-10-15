3bdab318e9545e7f9cce43c1aea2facb98b4191f
Guti <gutierrez.matias@outlook.com>
2021-10-15 20:18:57 -0300

    add redux-toolkit and counter example

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.global.css';
+import Counter from './container/Counter/Counter';
 
 const Hello = () => {
   return (
@@ -9,33 +10,7 @@ const Hello = () => {
       <div className="Hello">
         <img width="200px" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+      <Counter />
     </div>
   );
 };
---
+++ src/renderer/container/Counter/Counter.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useAppSelector, useAppDispatch } from '../../hooks';
+import { decrement, increment } from './counterSlice';
+
+export default function Counter() {
+  // The `state` arg is correctly typed as `RootState` already
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <div className="flex flex-col items-center">
+        <button
+          type="button"
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+          className="text-black w-full"
+        >
+          Increment
+        </button>
+        <div className="text-xl py-2">{count}</div>
+        <button
+          type="button"
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+          className="text-black w-full"
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
