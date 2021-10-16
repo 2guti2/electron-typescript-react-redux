66cd3989931dd5e1cf3b06f48884e3fc8a8c7b0e
Guti <gutierrez.matias@outlook.com>
2021-10-16 02:50:30 -0300

    add steps in counter

--- src/renderer/containers/Counter/index.tsx
+++ src/renderer/containers/Counter/index.tsx
@@ -1,32 +1,69 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { useAppSelector, useAppDispatch } from '../../store/hooks';
-import { decrement, increment } from './counterSlice';
+import {
+  increment,
+  incrementByAmount,
+  decrement,
+  decrementByAmount,
+} from './counterSlice';
 
 export default function Counter() {
   // The `state` arg is correctly typed as `RootState` already
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
+  const [step, setStep] = useState(1);
+  const onChangeStep = (event: ChangeEvent<HTMLInputElement>) =>
+    setStep(+event.target.value);
 
   return (
     <div>
       <div className="flex flex-col items-center">
         <button
           type="button"
-          aria-label="Increment value"
+          aria-label="+1"
           onClick={() => dispatch(increment())}
+          className="text-black w-full m-2"
+        >
+          +1
+        </button>
+        <button
+          type="button"
+          aria-label="Increment value"
+          onClick={() => dispatch(incrementByAmount(step))}
           className="text-black w-full"
         >
-          Increment
+          Increment by step
         </button>
-        <div className="text-xl py-2">{count}</div>
+        <div className="text-3xl py-2">{count}</div>
         <button
           type="button"
           aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
+          onClick={() => dispatch(decrementByAmount(step))}
           className="text-black w-full"
         >
-          Decrement
+          Decrement by step
         </button>
+        <button
+          type="button"
+          aria-label="-1"
+          onClick={() => dispatch(decrement())}
+          className="text-black w-full m-2"
+        >
+          -1
+        </button>
+      </div>
+      <div className="py-4 text-center">
+        <label htmlFor="step">
+          Step
+          <br />
+          <input
+            className="text-black text-center"
+            type="number"
+            name="step"
+            defaultValue={step}
+            onChange={onChangeStep}
+          />
+        </label>
       </div>
     </div>
   );
